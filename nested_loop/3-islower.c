564e8668732ae3a2e59692a105a86ef025fe87f8
p-prime1 <preciousnwaka95@gmail.com>
2023-05-25 22:59:04 +0100

    Fixed the islower function

--- nested_loop/3-islower.c
+++ nested_loop/3-islower.c
@@ -4,6 +4,17 @@ int main (void)
 {
 	_islower('H');
 	_islower('c');
+	_islower('o');
+	_islower(108);
+	_islower('c');
+	_islower('I');
+	_islower('s');
+	_islower(70);
+	_islower(20);
+	_islower('U');
+	_islower('n');
+	_islower(0);
+	_islower('H');
 	return 0;
 }
 int _islower(int c)
@@ -11,7 +22,7 @@ int _islower(int c)
 	int i;
 	int j;
 
-	for (j = 'A'; j <= 'Z'; j++)
+	/*for (j = 'A'; j <= 'Z'; j++)
 	{
 		if (c == j)
 		{
@@ -36,5 +47,14 @@ int _islower(int c)
 				continue;
 			}
 		}
+		*/
+	if (c >= 'a' && c <= 'z')
+	{
+		_putchar(1 + '0');
+	}
+	else
+	{
+		_putchar(0 + '0');
+	}
 	_putchar('\n');
 }
