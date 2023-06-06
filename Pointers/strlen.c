b59934063d351d9680efee8e2a4442db0ecfe9bb
p-prime1 <preciousnwaka95@gmail.com>
2023-06-06 22:08:28 +0100

    created and compiled strlen.c successfully

---
+++ Pointers/strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; ++i);
+	return (i);
+}
+
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
