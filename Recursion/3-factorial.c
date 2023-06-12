d364f470ffa0ae77d5e772e2a1c83af2c2a95e5e
p-prime1 <preciousnwaka95@gmail.com>
2023-06-12 20:13:12 +0100

    l

---
+++ Recursion/3-factorial.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+	}
+}
+
+int main(void)
+{
+    int r;
+
+    r = factorial(1);
+    printf("%d\n", r);
+    r = factorial(5);
+    printf("%d\n", r);
+    r = factorial(10);
+    printf("%d\n", r);
+    r = factorial(-1024);
+    printf("%d\n", r);
+    return (0);
+}
+
--- Recursion/strlen_recursion.c
+++ Recursion/strlen_recursion.c
@@ -2,18 +2,14 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
-
 	if (*s == '\0')
 	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-		i++;
-		_strlen_recursion(s + 1);
+		return(1 + _strlen_recursion(s + 1));
 	}
-	return (i);
 }
 
 int main(void)
